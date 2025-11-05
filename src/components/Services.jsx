5a7231d4cf190462b46887432f67038e78c9be2b
amit Kumar <amit.k001122@gmail.com>
2025-11-05 10:07:05 +0530

    Services Section minor bug fixes

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -15,49 +15,42 @@ const services = [
     title: "Pre-Purchase Home Inspections",
     description:
       "A detailed inspection of the property before purchase to identify issues and ensure you make an informed, confident investment decision.",
-    button: "Learn More",
   },
   {
     icon: <FaBuilding className="text-3xl text-[#385f82]" />,
     title: "Commercial Property Inspections",
     description:
       "Comprehensive evaluation of commercial spaces to assess structural integrity, safety systems, and maintenance needs—helping protect your business investment.",
-    button: "Learn More",
   },
   {
     icon: <FaHardHat  className="text-3xl text-[#385f82]" />,
     title: "New Construction Inspections",
     description:
       "Step-by-step inspections during construction to ensure quality, safety, and code compliance before your new home is completed.",
-    button: "Learn More",
   },
   {
     icon: <FaClipboardCheck  className="text-3xl text-[#385f82]" />,
     title: "11-Month Warrenty Inspection",
     description:
       "A complete inspection before your builder’s 1-year warranty expires to identify defects and ensure timely repairs at no extra cost to you.",
-    button: "Learn More",
   },
   {
     icon: <FaFileInvoice  className="text-3xl text-[#385f82]" />,
     title: "4-Point Insurance Inspection",
     description:
       "Focused inspection covering the roof, HVAC, plumbing, and electrical systems — required by insurance companies for older homes.",
-    button: "Learn More",
   },
   {
     icon: <FaMoneyCheckAlt  className="text-3xl text-[#385f82]" />,
     title: "Construction Bank Draw Inspections",
     description:
       "Detailed progress inspections for lenders and builders to verify completed work before each construction payment draw.",
-    button: "Learn More",
   },
   {
     icon: <FaCogs  className="text-3xl text-[#385f82] " />,
     title: "Phase Inspections",
     description:
       "Inspection at each critical construction phase—foundation, framing, and final—to ensure quality workmanship and code compliance.",
-    button: "Learn More",
   },
 ];
 
@@ -88,11 +81,7 @@ const Services = () => {
                 </h3>
               </div>              
               <p className="text-gray-600 text-base mb-4">{service.description}</p>
-              <div className="flex justify-center">
-              <button className="bg-[#385f82] w-[50%] text-white py-2 px-4 rounded-md hover:bg-[#2f4f6d] transition">
-                {service.button}
-              </button>
-              </div>
+              
             </div>
           ))}
         </div>
